9cd0f9ccf87a18ccb1f6200f35d368d4626795ed
kamilajesus <kamilaalves727@gmail.com>
2022-10-26 00:37:00 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório! '));
+}
+
+function pegaArquivo(caminhoDoArquivo){
+  const encoding = 'utf-8';
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if(erro) {
+      trataErro(erro);
+    }
+    console.log(chalk.green(texto));
+  })
+}
+
+pegaArquivo('./arquivos/texto.md');
