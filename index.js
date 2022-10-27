65e02e2f3f9001bbd63a270ac6f934e9b482827b
kamilajesus <kamilaalves727@gmail.com>
2022-10-27 00:30:17 -0300

    capturando links

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function trataErro(erro) {
-  throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório! '));
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados;
 }
 
-// Async/await
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// async/await
 
 async function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8';
   try {
+    const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto))
-  } catch(erro) {
-    trataErro(erro);
-  } finally {
-    console.log(chalk.yellow('operação concluída'));
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro)
   }
- }
-
-
-// Promises com then()
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(chalk.green(texto)))
-//     .catch(trataErro)
-// }
+}
 
 pegaArquivo('./arquivos/texto.md');
